ac48fc515266aea610bfd9bc9c48297bd1330f3f
J. Protopopovs <jprotopopov1122@gmail.com>
2018-09-19 20:23:53 +0300

    Lab 2

---
+++ src/Lab2/Solution.java
@@ -0,0 +1,42 @@
+package Lab2;
+
+import java.util.function.Function;
+import java.util.stream.IntStream;
+
+public class Solution {
+
+    public static long superDigit(long number) throws Exception {
+        if (number < 0) {
+            throw new Exception("Number must be positive or zero");
+        } else if (number < 10) {
+            return number;
+        } else {
+            return superDigit(superDigit(number / 10) + number % 10);
+        }
+    }
+
+    private static int powerSumHelper(int number, int power, int start)
+    {
+        if (number == 0) {
+            return 1;
+        } else {
+            final int root = (int) Math.ceil(Math.pow(number, 1.0 / power));
+            final Function<Integer, Integer> subtractPowerOf = i -> number - (int) Math.pow(i, power);
+
+            return IntStream.rangeClosed(start, root)
+                    .filter(i -> subtractPowerOf.apply(i) >= 0)
+                    .map(i -> powerSumHelper(subtractPowerOf.apply(i), power, i + 1))
+                    .sum();
+        }
+    }
+
+    public static int powerSum(int number, int power) throws Exception {
+        if (number < 1) {
+            throw new Exception("Number must be 1 or greater");
+        } else if (power < 2) {
+            throw new Exception("Power must be 2 or greater");
+        } else {
+            return powerSumHelper(number, power, 1);
+        }
+    }
+}
---
+++ src/Lab2/SolutionTest.java
@@ -0,0 +1,84 @@
+package Lab2;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    private void testSuperDigitFor(int number, int superDigit) throws Exception {
+        assertEquals(Solution.superDigit(number), superDigit);
+    }
+
+    private void testPowerSumFor(int number, int power, int count) throws Exception {
+        assertEquals(Solution.powerSum(number, power), count);
+    }
+
+    @Test
+    public void testSuperDigitFor9875() throws Exception {
+        testSuperDigitFor(9875, 2);
+    }
+
+    @Test
+    public void testSuperDigitFor1547() throws  Exception {
+        testSuperDigitFor(1547, 8);
+    }
+
+    @Test
+    public void testSuperDigitFor9999() throws  Exception {
+        testSuperDigitFor(9999, 9);
+    }
+
+    @Test
+    public void testSuperDigitFor983() throws Exception {
+        testSuperDigitFor(983, 2);
+    }
+
+    @Test
+    public void testSuperDigitFor5() throws Exception {
+        testSuperDigitFor(5, 5);
+    }
+
+    @Test
+    public void testSuperDigitFor0() throws Exception {
+        testSuperDigitFor(0, 0);
+    }
+
+    @Test
+    public void testSuperDigitFor1234() throws Exception {
+        testSuperDigitFor(1234, 1);
+    }
+
+    @Test
+    public void testSuperDigitFor4321() throws Exception {
+        testSuperDigitFor(4321, 1);
+    }
+
+    @Test
+    public void testPowerSumFor100_2() throws Exception {
+        testPowerSumFor(100, 2, 3);
+    }
+
+    @Test
+    public void testPowerSumFor100_3() throws Exception {
+        testPowerSumFor(100, 3, 1);
+    }
+
+    @Test
+    public void testPowerSumFor13_2() throws Exception {
+        testPowerSumFor(13, 2, 1);
+    }
+
+    @Test
+    public void testPowerSumFor10_2() throws Exception {
+        testPowerSumFor(10, 2, 1);
+    }
+
+    @Test
+    public void testPowerSumFor7_2() throws Exception {
+        testPowerSumFor(7, 2, 0);
+    }
+
+    @Test
+    public void testPowerSumFor17_4() throws Exception {
+        testPowerSumFor(17, 4, 1);
+    }
+}
